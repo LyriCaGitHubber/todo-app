f51f115818f0ee9c71ac0e721536b4abd7ce167e
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-22 12:32:15 +0200

    Create + button component. Add css stylings and import svg

---
+++ src/components/button/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+import buttonPlus from "./button.html";
+
+export default {
+  title: "Components/Button",
+  parameters: { layout: "fullscreen" },
+};
+
+export const btnSquare = () => buttonPlus;
