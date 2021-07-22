5d122c25424367b4ae031d77360184fc5ceb708c
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-22 16:02:32 +0200

    Create form component with first html structure

---
+++ src/components/formular/formular.stories.js
@@ -0,0 +1,9 @@
+import "./formular.css";
+import form from "./formular.html";
+
+export default {
+  title: "Components/Formular",
+  parameters: { layout: "centered" },
+};
+
+export const formular = () => form;
