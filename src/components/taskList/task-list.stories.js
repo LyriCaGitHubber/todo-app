afb7df8770631049b224287a61be700d8ebec117
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-25 12:58:44 +0200

    Add task list files and task list pages files

---
+++ src/components/taskList/task-list.stories.js
@@ -0,0 +1,9 @@
+import "./task-list.css";
+import taskList from "./task-list.html";
+
+export default {
+  title: "Components/Task-List",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskItems = () => taskList;
