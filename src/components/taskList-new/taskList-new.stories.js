b09f1154e5b96001d5dc66982859b2cd98195fd8
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-26 14:48:14 +0200

    Add new taskList files

---
+++ src/components/taskList-new/taskList-new.stories.js
@@ -0,0 +1,9 @@
+import "./taskList-new.css";
+import taskList from "./taskList-new.html";
+
+export default {
+  title: "Components/TaskListNew",
+  parameters: { layout: "fullscreen" },
+};
+
+export const regular = () => taskList;
