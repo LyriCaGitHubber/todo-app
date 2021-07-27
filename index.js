81b82c2c07931d03602a41f2cdda8939ff1bf5ab
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-27 16:47:12 +0200

    Create TaskList item function

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let alertMessage = "Hello World";
+const menuButton = document.querySelector(".iconButton");
+menuButton.onclick = sendAlert;
 
-alert(alertMessage);
+function sendAlert() {
+  alert("Hello World!");
+}
+
+const taskList = document.querySelector(".taskList");
+
+const taskOne = createTaskListItem();
+
+taskList.append(taskOne);
+
+function createTaskListItem() {
+  const taskListItem = document.createElement("label");
+  taskListItem.className = "taskItem";
+  const taskListInput = document.createElement("input");
+  taskListInput.className = "taskItem__checkbox";
+  taskListInput.setAttribute("type", "checkbox");
+  const taskListSpan = document.createElement("span");
+  taskListSpan.className = "taskItem__labelText";
+  taskListSpan.innerText = "New Task";
+  taskListItem.append(taskListInput, taskListSpan);
+  return taskListItem;
+}
+
+console.log(createTaskListItem());
