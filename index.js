f81c8ff63d2c9821ef202ddd0df10a88e718a126
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-28 10:05:02 +0200

    Change name of variables

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const taskOne = createTaskListItem();
 taskList.append(taskOne);
 
 function createTaskListItem() {
-  const taskListItem = document.createElement("label");
-  taskListItem.className = "taskItem";
-  const taskListInput = document.createElement("input");
-  taskListInput.className = "taskItem__checkbox";
-  taskListInput.setAttribute("type", "checkbox");
-  const taskListSpan = document.createElement("span");
-  taskListSpan.className = "taskItem__labelText";
-  taskListSpan.innerText = "New Task";
-  taskListItem.append(taskListInput, taskListSpan);
-  return taskListItem;
+  const label = document.createElement("label");
+  label.className = "taskItem";
+  const input = document.createElement("input");
+  input.className = "taskItem__checkbox";
+  input.setAttribute("type", "checkbox");
+  const span = document.createElement("span");
+  span.className = "taskItem__labelText";
+  span.innerText = "New Task";
+  label.append(input, span);
+  return label;
 }
 
 console.log(createTaskListItem());
