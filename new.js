9ab8f29ba9cec1cf5fd6b40b9a64d95dca70efe4
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-28 16:12:39 +0200

    just an adjustment

--- new.js
+++ new.js
@@ -4,7 +4,6 @@ form.onsubmit = function (event) {
   const newTask = { task: "", date: "", done: false };
   newTask.task = document.getElementById("enterTask").value;
   newTask.date = document.querySelector(".when_btn:checked").value;
-  newTask.done = true;
 
   console.log(newTask, newTask.task, newTask.date, newTask.done);
 };
