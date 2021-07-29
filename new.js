14397651d03b836957775bb10e7698ce3d1d180d
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-29 20:27:25 +0200

    Optimize new.js

--- new.js
+++ new.js
@@ -1,9 +1,13 @@
 const form = document.querySelector(".task-form");
 form.onsubmit = function (event) {
   event.preventDefault();
-  const newTask = { task: "", date: "", done: false };
-  newTask.task = document.getElementById("enterTask").value;
-  newTask.date = document.querySelector(".when_btn:checked").value;
+  const newTask = { taskTitle: "", date: "", done: false };
 
-  console.log(newTask, newTask.task, newTask.date, newTask.done);
+  const dateInputChecked = document.querySelector(".when_btn:checked").value;
+  const taskContent = document.getElementById("enterTask").value;
+
+  newTask.taskTitle = taskContent;
+  newTask.date = dateInputChecked;
+
+  console.log(newTask);
 };
