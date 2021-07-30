1c3e52cc6bcf9a6df90772099a7b38e896684aad
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-07-30 20:45:42 +0200

    Create date radio buttons. Add onclick function to show respective tasks

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 import { getLocalStorage } from "./util/localStorage.js";
 
 const Tasks = getLocalStorage("tasks", []);
+console.log(Tasks);
 const taskItems = Tasks.map((task) => createTaskListItem(task));
-
 const taskList = document.querySelector(".taskList");
 
 taskList.append(...taskItems);
-console.log(taskItems);
+
+//Date Buttons
 const btnToday = document.querySelector("#today");
 const btnTomorrow = document.querySelector("#tomorrow");
 const btnSelectDate = document.querySelector("#selectDate");
 
 btnToday.onclick = () => {
   taskList.innerHTML = "";
-  const todayFilter = taskItems.filter(
-    (item) => item.className == "taskItem.today"
-  );
-  console.log(todayFilter);
+  const taskListToday = Tasks.filter((today) => today.date === "Today");
+  const todayItems = taskListToday.map((task) => createTaskListItem(task));
+  taskList.append(...todayItems);
 };
 
+btnTomorrow.onclick = () => {
+  taskList.innerHTML = "";
+  const tasksTomorrow = Tasks.filter(
+    (tomorrow) => tomorrow.date === "Tomorrow"
+  );
+  const tomorrowItems = tasksTomorrow.map((task) => createTaskListItem(task));
+  taskList.append(...tomorrowItems);
+};
+
+// Creates html dom for list items
 function createTaskListItem(task) {
   const label = document.createElement("label");
   label.className = "taskItem";
